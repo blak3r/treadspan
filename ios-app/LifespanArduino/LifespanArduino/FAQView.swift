3bfcbc1b2d68982becd2f81b5244aafdca2382c1
Blake Robertson <blake.robertson@gmail.com>
2025-02-23 16:47:50 -0500

    FAQ View works now

--- ios-app/LifespanArduino/LifespanArduino/FAQView.swift
+++ ios-app/LifespanArduino/LifespanArduino/FAQView.swift
@@ -3,85 +3,57 @@ import SwiftUI
 struct FAQItem: Identifiable {
     let id = UUID()
     let question: String
-    let answer: AnyView // Change Text to AnyView for flexibility
+    let answer: AnyView
 }
 
 struct FAQView: View {
     @State private var expandedQuestion: UUID?
-
+    
     let faqItems: [FAQItem] = [
         FAQItem(
             question: "Is this app affiliated with LifeSpan Fitness?",
             answer: AnyView(
-                Text("No. This app is in no way associated with LifeSpan Fitness. It was built by Blake Robertson (a LifeSpan customer).")
+                Text("No. This app is in no way associated with LifeSpan Fitness. It was built by Blake Robertson (a LifeSpan customer) that wanted a more seamless way to track his steps.")
             )
         ),
         FAQItem(
-            question: "Do I need anything else beside the app for it to work?",
+            question: "Do I need anything else besides the app for it to work?",
             answer: AnyView(
-                HStack {
-                    Text("Yes, in order to log multiple sessions you need to buy a ~$17 module.")
-                }
+                Text("Yes, in order to log multiple sessions you need to buy a ~$17 module.")
             )
         ),
         FAQItem(
             question: "How do I submit feature requests / support?",
             answer: AnyView(
-                HStack {
-                    Text("Go to ")
-                    Link("", destination: URL(string: "https://github.com/blak3r/treadspan/discussions")!)
-                        .foregroundColor(.blue)
-                        .underline()
-                }
+                Text("Go to the [TreadSpan Discussion Page](https://github.com/blak3r/treadspan/discussions) for feature requests and support.")
             )
         ),
         FAQItem(
             question: "Why do you need READ steps permission?",
             answer: AnyView(
-                HStack {
-                    Text("Read permission is needed for the metrics view to work.  Our metrics view is very similar to the Apple Health Steps graphs but adds a data series that shows how many steps you take on a treadmill.  To do this we need to read the number of steps you got from other data sources as well.")
-                }
+                Text("Read permission is needed for the metrics view to work. Our metrics view is similar to the Apple Health Steps graphs, but adds a data series showing how many steps you take on a treadmill. To do this, we need to read steps data from other sources as well.")
             )
         ),
         FAQItem(
             question: "Is any information shared with 3rd Parties?",
             answer: AnyView(
-                HStack {
-                    Text("Absolutely not. The app does report any information to 3rd parties.  All information is stored in Apple HealthKit.")
-                }
+                Text("Absolutely not. The app does not report any information to 3rd parties. All information is stored in Apple HealthKit.")
             )
         ),
         FAQItem(
             question: "Can I still use LifeSpan Fit?",
             answer: AnyView(
-                HStack {
-                    Text("In order to the Treadspan solution to work there is a chip that connects to your LifeSpan Omni Console.  This chip uses the same Bluetooth connection that the Lifespan Mobile App uses.  As such, as long as the chip is powered on it'll occupy the connection and therefore the original LifeSpan Mobile App will not be able to connect. If you power down the chip by unplugging it's usb-c connector, then you will notice that the bluetooth icon on the console will go grey or disappear.  You will then be able to connect the original LifeSpan app.")
-                }
+                Text("In order for the Treadspan solution to work, there is a chip that connects to your LifeSpan Omni Console. This chip uses the same Bluetooth connection that the LifeSpan Mobile App uses. As long as the chip is powered on, it will occupy the connection so the original app cannot connect. Once you power down the chip by unplugging its USB-C connector, the Bluetooth icon on the console will go grey or disappear, and you will be able to connect the original LifeSpan app.")
             )
         ),
         FAQItem(
             question: "Which model treadmills are supported?",
             answer: AnyView(
-                HStack {
-                    Text("Currently, the solution has been tested and developed to work with the ")
-                    Link("", destination: URL(string: "https://amzn.to/4bbn8ok")!)
-                        .foregroundColor(.blue)
-                        .underline()
-                    Text("It should work with any LifeSpan treadmill that works with the Omni Console.  If you have the RETRO console, there is a solution that will work as well but the hardware portion requires some assembly. It's probably out of reach if you're not a tinkerer");
-                    
-                }
+                Text("Currently, the solution has been tested with the [supported treadmill](https://amzn.to/4bbn8ok). It should work with any LifeSpan treadmill that works with the Omni Console. If you have the RETRO console, there is also a solution, but the hardware requires some assembly and is probably out of reach if you're not a tinkerer.")
             )
-        ),
-        FAQItem(
-            question: "What Treadmills are Supported?",
-            answer: Text("Currently, the solution has been tested and developed to work with the ")
-            +
-                    Text("It should work with any LifeSpan treadmill that works with the Omni Console (TR1000, TR1200, & TR5000).  If you have the RETRO console, there is a solution that will work as well but the hardware portion requires some assembly. It's probably out of reach if you're not a tinkerer")
-                    
-        ),
-        
+        )
     ]
-
+    
     var body: some View {
         NavigationView {
             List {
